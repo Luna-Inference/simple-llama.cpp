ef4c9688a308b6bb11f7291555ce52b65d0c3de8
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-04-24 12:50:40 +0000

    Modified .gitignore to ignore build/ folder

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 3;
-char const *LLAMA_COMMIT = "822f5ec";
+int LLAMA_BUILD_NUMBER = 5;
+char const *LLAMA_COMMIT = "cda6515";
 char const *LLAMA_COMPILER = "cc (Debian 12.2.0-14) 12.2.0";
 char const *LLAMA_BUILD_TARGET = "aarch64-linux-gnu";
