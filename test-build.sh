2d17e8c57f999336885afe4b0e1a33cebfbb3c70
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-04-24 16:48:05 +0000

    idk what's wrong anymore

--- test-build.sh
+++ test-build.sh
@@ -16,6 +16,15 @@ fi
 echo 'running test'
 ./build/bin/llama-cli -m ./gguf/Llama-3.2-1B.Q8_0.gguf -p "Hello, how are you?" -n 128 -no-cnv | tee test_output.txt
 
+# Debug GH_PAGES_TOKEN and clone URL
+if [ -z "$GH_PAGES_TOKEN" ]; then
+  echo "ERROR: GH_PAGES_TOKEN is not set"
+else
+  echo "GH_PAGES_TOKEN length: ${#GH_PAGES_TOKEN}"
+fi
+
+echo "About to clone pages repo with URL: https://x-access-token:${GH_PAGES_TOKEN}@github.com/Luna-Inference/Luna-inference.github.io.git"
+
 # Publish test output to GitHub Pages
 git clone https://x-access-token:${GH_PAGES_TOKEN}@github.com/Luna-Inference/Luna-inference.github.io.git gh-pages
 cd gh-pages
@@ -24,6 +33,6 @@ cat ../test_output.txt >> index.html
 echo '</pre>' >> index.html
 git config user.name "tungvunguyennguyen@gmail.com"
 git config user.email "tungvunguyennguyen@gmail.com"
-git add index.html
+git add .
 git commit -m "Update index.html with latest test output"
 git push https://x-access-token:${GH_PAGES_TOKEN}@github.com/Luna-Inference/Luna-inference.github.io.git main
