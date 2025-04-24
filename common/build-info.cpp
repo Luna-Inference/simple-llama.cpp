7c9cbf1cc0309965d36cd30de8351d4853bf8900
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-04-24 14:11:31 +0000

    Adding github actions

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 7;
-char const *LLAMA_COMMIT = "8b24971";
+int LLAMA_BUILD_NUMBER = 8;
+char const *LLAMA_COMMIT = "fd5f5b6";
 char const *LLAMA_COMPILER = "cc (Debian 12.2.0-14) 12.2.0";
 char const *LLAMA_BUILD_TARGET = "aarch64-linux-gnu";
