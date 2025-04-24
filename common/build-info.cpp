e75ee7e2ffff029fc32db1fdc4ec44741433d9f2
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-04-24 14:16:40 +0000

    Added dependencies installation

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 8;
-char const *LLAMA_COMMIT = "fd5f5b6";
+int LLAMA_BUILD_NUMBER = 9;
+char const *LLAMA_COMMIT = "7c9cbf1";
 char const *LLAMA_COMPILER = "cc (Debian 12.2.0-14) 12.2.0";
 char const *LLAMA_BUILD_TARGET = "aarch64-linux-gnu";
