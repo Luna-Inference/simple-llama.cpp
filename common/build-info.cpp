822f5ecdf76056b1318bdb2a30e523919e35796d
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-04-24 03:55:48 +0000

    Removed cmake windows & mac

--- common/build-info.cpp
+++ common/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 2;
-char const *LLAMA_COMMIT = "efa96b3";
+char const *LLAMA_COMMIT = "4d05c16";
 char const *LLAMA_COMPILER = "cc (Debian 12.2.0-14) 12.2.0";
 char const *LLAMA_BUILD_TARGET = "aarch64-linux-gnu";
